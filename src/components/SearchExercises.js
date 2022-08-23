cbc338ed25d5140747391a5628a38b47d7d96aa7
kenertj <tsoojkk@gmail.com>
2022-08-23 11:52:34 +0300

    Data fetching and search functionality

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
-
+import { exerciseOptions, fetchData } from '../utils/fetchData';
 const SearchExercises = () => {
     const [search, setSearch] = useState('')
+    const [exercises, setExercises] = useState([])
+    const [bodyParts, setBodyParts] = useState([])
+
+    useEffect(() => {
+        const fetchExercisesData = async () => {
+            const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+
+            setBodyParts(['all', ...bodyPartsData]);
+        }
+        fetchExercisesData();
+    }, [])
+
+
+    const handleSearch = async () => {
+        if (search) {
+            const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+
+            const searchedExercises = exercisesData.filter((exercise) =>
+                exercise.name.toLowerCase().includes(search)
+                || exercise.target.toLowerCase().includes(search)
+                || exercise.equipment.toLowerCase().includes(search)
+                || exercise.bodyPart.toLowerCase().includes(search)
+
+            );
+            setSearch('');
+            setExercises(searchedExercises);
+        }
+    }
     return (
         <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
             <Typography fontWeight={700} sx={{
@@ -41,6 +69,15 @@ const SearchExercises = () => {
                 >
                     Search
                 </Button>
+            </Box>
+            <Box sx={{
+                positsion: 'relative',
+                width: '100%',
+                p: '20px'
+
+            }}>
+
+
             </Box>
         </Stack>
     )
